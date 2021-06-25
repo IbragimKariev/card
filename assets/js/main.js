8b815ce252d1ffb681c7c6d413254ae65002d156
ibragimKariev <ibrosh_96@mail.ru>
2021-06-25 09:07:44 +0600

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    // add all to same gallery
+    $(".gallery a").attr("data-fancybox","mygallery");
+    // assign captions and title from alt-attributes of images:
+    $(".gallery a").each(function(){
+        $(this).attr("data-caption", $(this).find("img").attr("alt"));
+        $(this).attr("title", $(this).find("img").attr("alt"));
+    });
+    // start fancybox:
+    $(".gallery a").fancybox();
+});
+
+
+
+(function ($) {
+
+    /*------------------
+        Preloader
+    --------------------*/
+    $(window).on('load', function () {
+        $(".loader").fadeOut();
+        $("#preloder").delay(200).fadeOut("slow");
+
+    });
+
+
+})(jQuery);
